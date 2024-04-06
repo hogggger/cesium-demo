816b6e88569dec0d0cd10c29fdb29134222e2ca5
hogger <deuntlt@vip.qq.com>
2024-04-06 22:19:11 +0800

    feat:1.before init cesium

--- src/views/DemoView.vue
+++ src/views/DemoView.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="demo-container">
+  <div class="demo-container" id="cesiumId" :ref="cesiumRef">
     <h1>Demo about cesiumJS</h1>
   </div>
 </template>
+<script>
+import { ref } from 'vue'
+const cesiumRef = ref(null)
+const initCesiumFunc = ()=>{
+    
+}
+</script>
 
 <style scoped>
 @media (min-width: 1024px) {
