f241489425d9367b57159ddbae826fbe53d5d65a
hogger <deuntlt@vip.qq.com>
2024-04-06 16:53:44 +0800

    feat:1. install cesium and lock version 2.create file about demo-cesium

---
+++ src/views/DemoView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="demo-container">
+    <h1>Demo about cesiumJS</h1>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
